770bc1e5e5d8a5dbad9610037ed66980fc987959
Kuznetsov Andrey <and.rey.q@yandex.ru>
2019-11-30 22:18:32 +0300

    2, 3

--- index.js
+++ index.js
@@ -6,3 +6,29 @@
   typeof {} // "object"
   typeof null // "object" !
   typeof function(){} || (() => {}) // "function"
+
+2. Вывести числа, находящиеся под главной диагональю матрицы
+const matrix = [
+  [3, 2, 1],
+  [6, 5, 4],
+  [9, 7, 8],
+]
+const log = console.log
+matrix.forEach((item, outerIndex) => {
+  item.forEach((item, innerIndex) => {
+    if (outerIndex > innerIndex) log(item)
+  })
+})
+
+3. Максимальное и минимальное значение в массиве
+const arr = [1, -2, 3, 0, -8]
+let searchInArray = {
+  min: (arr) => {
+    return Math.min(...arr)
+  },
+  max: (arr) => {
+    return Math.max(...arr)
+  }
+}
+searchInArray.min(arr) // -8
+searchInArray.max(arr) // 3
